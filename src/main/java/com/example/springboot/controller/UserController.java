640a2b17a8c40e1668130bb9d5c90b6a06be8bf9
Konstantin <romanov.konst96@gmail.com>
2020-10-01 18:15:54 +0300

    commit new

--- src/main/java/com/example/springboot/controller/UserController.java
+++ src/main/java/com/example/springboot/controller/UserController.java
@@ -26,10 +26,12 @@ public class UserController {
     }
 
     @GetMapping("/admin")
-    public String findAll(Model model){
+    public String findAll(Model model, Principal principal){
         List<User> users = userService.findAll();
+        User currentUser = userService.findUserByFirstName(principal.getName());
         model.addAttribute("users", users);
         model.addAttribute("user", new User());
+        model.addAttribute("currentUser", currentUser);
         return "user-list";
     }
 
